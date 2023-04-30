aade9e590689d5caa27de9a64d81585c5541e614
Janders8 <janekzmuda8@gmail.com>
2023-05-01 00:56:15 +0200

    gpu and still working on disk

--- disk.py
+++ disk.py
@@ -5,26 +5,36 @@ import time
 class Disk:
     @staticmethod
     def disk_activity():
+        disk_io_counters1 = psutil.disk_io_counters()
+        disk_io_counters2 = psutil.disk_io_counters()
 
-        start = time.time()
-        disk_before = psutil.disk_io_counters()
+        read_count1 = disk_io_counters1.read_count
+        write_count1 = disk_io_counters1.write_count
+        read_bytes1 = disk_io_counters1.read_bytes
+        write_bytes1 = disk_io_counters1.write_bytes
+
+        # Wait for some time
         time.sleep(1)
-        end = time.time()
-        disk_after = psutil.disk_io_counters()
 
+        disk_io_counters2 = psutil.disk_io_counters()
 
-        read_bytes = disk_after.read_bytes - disk_before.read_bytes
-        write_bytes = disk_after.write_bytes - disk_before.write_bytes
+        read_count2 = disk_io_counters2.read_count
+        write_count2 = disk_io_counters2.write_count
+        read_bytes2 = disk_io_counters2.read_bytes
+        write_bytes2 = disk_io_counters2.write_bytes
 
-        disk_time = end - start
+        # Calculate the number of read and write operations and bytes transferred
+        read_ops = read_count2 - read_count1
+        write_ops = write_count2 - write_count1
+        read_bytes = read_bytes2 - read_bytes1
+        write_bytes = write_bytes2 - write_bytes1
 
-        print(psutil.disk_io_counters().read_time/ 1000000000)
-        print(psutil.disk_io_counters())
+        # Calculate the total number of operations and bytes transferred
+        total_ops = read_ops + write_ops
+        total_bytes = read_bytes + write_bytes
 
-
-
-        disk_activity_percent = (disk_before.read_time/1000000000 + disk_before.read_time/1000000000) / disk_time
-
+        # Calculate the percentage of disk activity
+        disk_activity_percent = (total_ops / psutil.cpu_count() / psutil.cpu_freq().current) * 100
 
         return disk_activity_percent
 
