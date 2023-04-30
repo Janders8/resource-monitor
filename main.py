d5a6971cd346955467bb29a98dfd40b7ec6df580
Janders8 <76881847+Janders8@users.noreply.github.com>
2023-04-30 15:58:41 +0200

    gui tests

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
 from ram import Ram
 from cpu import Cpu
+from test import *
+import sys
+from PyQt5.QtWidgets import (
+    QApplication, QDialog, QMainWindow, QMessageBox
+)
+
+
+class Window(QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.ui.textBrowser.setText("test")
+
 
 df = Cpu.initiateMonitor()
 
-while True:
+# while True:
+#
+#
+#     df.loc[len(df)] = Cpu.getThreadUsage()
+#
+#     print(df)
 
 
-    df.loc[len(df)] = Cpu.getThreadUsage()
+if __name__ == "__main__":
 
-    print(df)
+    app = QApplication(sys.argv)
+    win = Window()
+    win.show()
+    sys.exit(app.exec())
