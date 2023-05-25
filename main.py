43b93bbad57d269c59e978fe267afba29d9045c3
Janders8 <janekzmuda8@gmail.com>
2023-05-25 14:58:21 +0200

    first version of test selection

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ from PyQt5.QtCore import pyqtSignal
 
 from PyQt5.QtCore import QThread
 
+from testSelect import findTests
 
 class MyThread(QThread):
     my_signal = pyqtSignal(dict)
@@ -152,11 +153,21 @@ class Window(QMainWindow):
         self.my_thread.start()
 
         #initial values
+        # tests = findTests()
+        #
+        # for i, t in enumerate(tests.tests):
+        #     self.ui.comboBoxTests.addItem(f'{t}')
+        #
+
+
+
+
+
         _translate = QtCore.QCoreApplication.translate
 
         self.ui.textBrowserGpuName.setText(str(Gpu.getGpuName()))
         self.ui.textBrowserDiskModel.setText(str(Disk.get_hard_drive_model()))
-        self.ui.comboBoxTests.setItemText(0, _translate("MainWindow", "innyTekst"))
+        self.ui.comboBoxTests.setItemText(0, _translate("MainWindow", "cos"))
 
 
 
--- testSelect.py
+++ testSelect.py
@@ -1,2 +1,43 @@
 # todo
 # select all py files from folder "tests" and ...
+
+
+import os
+import time
+
+
+TESTS_PATH = "tests/"
+
+# path to arcgispro python
+arcgisproPythonPath = r'C:\"Program Files"\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python'
+class findTests():
+    def __init__(self):
+        self.tests = os.listdir(TESTS_PATH)
+        self.createDict()
+
+
+    def createDict(self):
+        self.testWithPaths = {}
+        for i in self.tests:
+            self.testWithPaths[i] = TESTS_PATH + i
+
+        #print(self.testWithPaths)
+
+    def runSelectedScript(self, testPath):
+        exec(open(TESTS_PATH+testPath).read())
+
+    def runUsingSpecyficPython(self, testPath):
+        # tutaj mozna mierzyc czas
+        start = time.time()
+        os.system(fr'cmd /c {arcgisproPythonPath} {testPath}')
+
+        timeOfTest = time.time() - start
+
+        print("czas testu: ", timeOfTest)
+
+        return timeOfTest
+
+#
+# test = findTests()
+#
+# test.runUsingSpecyficPython(test.testWithPaths[test.tests[0]])
