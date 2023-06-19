3ac739dbb36744876e576b2f2e83de9c6e16a47e
Janders8 <76881847+Janders8@users.noreply.github.com>
2023-06-19 17:51:59 +0200

    added slopeTest

---
+++ tests/slopeTest.py
@@ -0,0 +1,17 @@
+
+# importing the module
+import arcpy
+
+try:
+    out_raster = arcpy.sa.Slope(r"tests\slopeTestData\dem.tiff", "PERCENT_RISE", 1, "PLANAR", "METER");
+    out_raster.save(r"tests\slopeTestData\slopeTest.gdb\Slope")
+except Exception as Argument:
+
+    # creating/opening a file
+    f = open("errorLog.txt", "a")
+
+    # writing in the file
+    f.write(str(Argument))
+
+    # closing the file
+    f.close()
